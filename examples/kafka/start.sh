858d34cdc91a4d5f9c8872f50c6863ed7cdba258
Simon Zambrovski <simon.zambrovski@holisticon.de>
2017-06-30 12:01:34 +0200

    Added params for zookeeper

--- examples/kafka/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export DOCKER_IP=`docker-machine ip \`docker-machine active\``
-
-docker run -p 2181:2181 -p 9092:9092 -d --env ADVERTISED_HOST=$DOCKER_IP --env ADVERTISED_PORT=9092 spotify/kafka
-
